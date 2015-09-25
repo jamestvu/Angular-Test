89f0f84dd8f809d04622e6564da16aefbde75fcf
James Vu <jamesvu@eworldes.com>
2015-09-24 20:40:54 -1000

    Initial commit.

---
+++ src/app.js
@@ -0,0 +1,8 @@
+﻿angular
+    .module('angularTestApp',
+    ['ui.router', 'angularTestApp.Routes', 'LoginModule', 'TasksModule']);
+
+angular
+    .module('angularTestApp')
+    .constant('applicationName', 'AngularTestApp')
+    .constant('versionNumber', '0.1');
---
+++ src/routes.js
@@ -0,0 +1,19 @@
+﻿(function () {
+    function Routes($stateProvider, $urlRouterProvider) {
+        $urlRouterProvider.otherwise('/login');
+
+        $stateProvider
+            .state('login', {
+                url: '/login',
+                templateUrl: 'modules/login/login.html'
+            })
+
+            .state('tasks', {
+                url: '/tasks',
+                templateUrl: 'modules/tasks/tasks.html'
+            });
+    }
+    angular
+        .module('angularTestApp.Routes', [])
+        .config(['$stateProvider', '$urlRouterProvider', Routes]);
+})();
